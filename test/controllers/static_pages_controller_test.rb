4085c7790b11ed27f29194d102826bf40a2d619f
aguspe <agustin.pe94@gmail.com>
2021-04-02 11:11:52 +0200

    Added guardfile

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = 'sample app'
+    @base_title = 'ruby base title sample app'
   end
 
   test "should get home root" do
@@ -13,7 +13,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', "home #{@base_title}"
+    assert_select 'title', "#{@base_title}"
   end
 
   test "should get help" do
