899a71ba1c4b0c8d4aee33fda389073370510c0c
aguspe <pequesitos@hotmail.es>
2021-04-12 22:28:38 +0200

    Added user signup tests

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'User cannot sign up with invalid data' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {
+        name: '',
+        email: 'agustin.wrongformat',
+        password: '12341234',
+        password_confirmation: '12341234'
+      } }
+    end
+  end
+
+  test 'User can succesfully sign up' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {
+        name: 'agustinTest',
+        email: 'agustin@email.com',
+        password: '12341234',
+        password_confirmation: '12341234'
+      } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
