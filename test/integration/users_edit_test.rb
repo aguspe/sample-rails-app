ab83f0fdefff9424cec035839934da428262ab21
aguspe <pequesitos@hotmail.es>
2021-04-18 00:07:58 +0200

    Added tests for edit

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:agustin)
+  end
+
+  test 'failed update' do
+    get edit_user_path @user
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: {
+      name: '',
+      email: 'failed',
+      password: 'hello',
+      password_confirmation: 'bye'
+    } }
+    assert_template 'users/edit'
+  end
+
+  test 'succesful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Agustin1'
+    email = 'agustin1@test.com'
+    patch user_path(@user), params: { user: {
+      name: 'Agustin1',
+      email: 'agustin1@test.com',
+      password: 'hello1',
+      password_confirmation: 'hello1'
+    } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+end
