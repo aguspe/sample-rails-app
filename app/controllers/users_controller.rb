4ba1196a88d1e9d8ecfc362af169f20894075fdb
aguspe <pequesitos@hotmail.es>
2021-04-21 22:39:34 +0200

    Added activation for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,8 +31,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        log_in @user
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        UserMailer.account_activation(@user).deliver_now
+        flash[:info] = 'Please check your email to activate your account'
+        format.html { redirect_to root_url, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
