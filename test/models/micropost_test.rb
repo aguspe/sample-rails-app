7db53b1779e5bc81086dc08f38e83d962a1247de
aguspe <pequesitos@hotmail.es>
2021-04-28 23:22:09 +0200

    Added limit caracter on micropost, extra tests and features

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,9 +1,9 @@
-require "test_helper"
+require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:agustin)
-    @microposts = Micropost.new(content: 'some content', user_id: @user.id)
+    @microposts = @user.microposts.build(content: 'some content')
   end
 
   test 'should be valid' do
@@ -13,5 +13,19 @@ class MicropostTest < ActiveSupport::TestCase
   test 'should not be valid' do
     @microposts.user_id = nil
     assert_not @microposts.valid?
+  end
+
+  test 'content should be present' do
+    @microposts.content = '  '
+    assert_not @microposts.valid?
+  end
+
+  test "length shouldn't be more than 100" do
+    @microposts.content = 'a' * 102
+    assert_not @microposts.valid?
+  end
+
+  test 'order should be the most recent first' do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 end
