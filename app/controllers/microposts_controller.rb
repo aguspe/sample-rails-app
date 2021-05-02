7ffc09344880ef5578fd7c4a06290bc207dac879
aguspe <pequesitos@hotmail.es>
2021-05-02 11:03:12 +0200

    Added ability to destroy microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,6 @@
 class MicropostsController < ApplicationController
+  before_action :require_log_in, only: %i[create destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -10,11 +12,20 @@ class MicropostsController < ApplicationController
     render 'static_pages/home'
   end
 
-  def destroy;end
+  def destroy
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+    redirect_to request.referrer || root_url
+  end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by_id(params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
